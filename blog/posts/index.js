b60f5436972aa6f2227fbd8e8deb5b23afdff6c1
LucianoPierdona <lucianompj1@gmail.com>
2020-12-20 22:20:22 -0300

    added cors

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 const posts = {};
 
 app.get("/posts", (req, res) => {
