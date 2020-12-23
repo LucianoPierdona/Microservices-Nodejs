175eccaaaeb58e2ec53966a8499e8253c616e840
LucianoPierdona <lucianompj1@gmail.com>
2020-12-22 23:07:42 -0300

    receiving events

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -29,6 +29,12 @@ app.post("/posts", async (req, res) => {
   return res.status(201).send(posts[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body.type);
+
+  return res.send({});
+});
+
 app.listen(4000, () => {
   console.log("Listening on port 4000");
 });
