cfee5929c38459dbb15c5a40b395d30a3d7265af
LucianoPierdona <lucianompj1@gmail.com>
2020-12-23 23:10:03 -0300

    updating comment content

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -38,7 +38,29 @@ app.post("/posts/:id/comments", async (req, res) => {
 });
 
 app.post("/events", (req, res) => {
-  console.log("Received event", req.body.type);
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, content, status, id } = data;
+
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+
+    await axios.post('http://localhost:4005/events', {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content
+      }
+    })
+  }
 
   return res.send({});
 });
