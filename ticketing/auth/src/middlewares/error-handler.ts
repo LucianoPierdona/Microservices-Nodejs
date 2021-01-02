6960a82612017b38c2650aa68412ed88237c754e
LucianoPierdona <lucianompj1@gmail.com>
2021-01-02 15:10:02 -0300

    returning error message

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -6,9 +6,7 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("something went wrong", err);
-
   return res.status(400).send({
-    message: "Something went wrong",
+    message: err.message,
   });
 };
