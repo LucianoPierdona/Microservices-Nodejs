bc8e87f4c053aefa8910ed5d625e58b576c9898a
LucianoPierdona <lucianompj1@gmail.com>
2021-01-03 15:23:18 -0300

    added useRequest hook

--- ticketing/client/pages/auth/signup.js
+++ ticketing/client/pages/auth/signup.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { useState } from "react";
+import useRequest from "../../hooks/useRequest";
 
 export default () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -41,16 +41,6 @@ export default () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      {errors.length > 0 && (
-        <div className="alert alert-danger">
-          <h4>Ops....</h4>
-          <ul className="my-0">
-            {errors.map((err) => (
-              <li key={err.message}>{err.message}</li>
-            ))}
-          </ul>
-        </div>
-      )}
       <button className="btn btn-primary">Sign up</button>
     </form>
   );
