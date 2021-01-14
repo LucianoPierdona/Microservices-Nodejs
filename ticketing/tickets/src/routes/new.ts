b9a1eb100fffc240ed5c20fca19c1ce35de68add
LucianoPierdona <lucianompj1@gmail.com>
2021-01-13 22:58:52 -0300

    added title validation

--- ticketing/tickets/src/routes/new.ts
+++ ticketing/tickets/src/routes/new.ts
@@ -1,10 +1,17 @@
-import { requireAuth } from '@lpjtickets/common';
+import { requireAuth, validateRequest } from '@lpjtickets/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  return res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [body('title').not().isEmpty().withMessage('Title is required')],
+  validateRequest,
+  (req: Request, res: Response) => {
+    return res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };
