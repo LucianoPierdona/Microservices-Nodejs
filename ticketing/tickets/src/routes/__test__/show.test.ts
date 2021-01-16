8c8c339e9d6c2497365163c6fed8a275e7502c99
LucianoPierdona <lucianompj1@gmail.com>
2021-01-15 22:00:10 -0300

    creating the showTicketRouter

--- ticketing/tickets/src/routes/__test__/show.test.ts
+++ ticketing/tickets/src/routes/__test__/show.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('returns a 404 if the ticket is not found', async () => {
-  await request(app).get('/api/tickets/owdkowkda').send().expect(404);
+  await request(app).get('/api/tickets/1').send().expect(404);
 });
 
 it('return the ticket if the ticket is found', async () => {
