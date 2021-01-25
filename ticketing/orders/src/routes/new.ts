4a6f31fc375d536840680b71671f5880ff36a2e8
LucianoPierdona <lucianompj1@gmail.com>
2021-01-24 22:44:23 -0300

    started new validation

--- ticketing/orders/src/routes/new.ts
+++ ticketing/orders/src/routes/new.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@lpjtickets/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  return res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  async (req: Request, res: Response) => {
+    return res.send({});
+  }
+);
 
 export { router as newOrdersRouter };
