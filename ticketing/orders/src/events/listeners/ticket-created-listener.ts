cac0088ce298094f185cea71d3c450cb070dee2a
LucianoPierdona <lucianompj1@gmail.com>
2021-01-26 10:30:14 -0300

    starting ticketCreatedListener

---
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,10 @@
+import { Listener, Subjects, TicketCreatedEvent } from '@lpjtickets/common';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = 'orders-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+}
