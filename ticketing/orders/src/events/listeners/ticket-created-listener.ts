c5d8060fd548b948cc1580bd58ab7f6ef8b3f76f
LucianoPierdona <lucianompj1@gmail.com>
2021-01-26 10:31:42 -0300

    created queueGroupName constant

--- ticketing/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -1,10 +1,11 @@
 import { Listener, Subjects, TicketCreatedEvent } from '@lpjtickets/common';
 import { Message } from 'node-nats-streaming';
 import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
