1a4b24d5f263742b868ec9e55c2549057b27bd51
LucianoPierdona <lucianompj1@gmail.com>
2021-01-25 23:40:49 -0300

    finished order and delete tests

--- ticketing/orders/src/routes/__test__/delete.test.ts
+++ ticketing/orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import { Order } from '../../models/order';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('successfully cancel the order', async () => {
   const ticket = Ticket.build({
@@ -29,4 +30,25 @@ it('successfully cancel the order', async () => {
   expect(cancelledOrder[0].status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo('emits an order cancelled event');
+it('emits an order cancelled event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+
+  await ticket.save();
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set(`Cookie`, user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
