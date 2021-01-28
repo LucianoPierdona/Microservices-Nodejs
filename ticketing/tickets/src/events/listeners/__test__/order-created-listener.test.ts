07d37f97e2e91ce95c07db8d69e714ced83fa2bf
LucianoPierdona <lucianompj1@gmail.com>
2021-01-27 22:12:08 -0300

    started order listener setup

---
+++ ticketing/tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,37 @@
+import { OrderCreatedEvent, OrderStatus } from '@lpjtickets/common';
+import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedListener } from '../order-created-listener';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 99,
+    userId: 'okdwo',
+  });
+
+  await ticket.save();
+
+  const data: OrderCreatedEvent['data'] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    status: OrderStatus.Created,
+    userId: 'dadwkdo',
+    expiresAt: 'dowdwodk',
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
