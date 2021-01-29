d96ea27fcf0b4a8d8c8b2ea8a27a86e817a081b1
LucianoPierdona <lucianompj1@gmail.com>
2021-01-28 21:49:20 -0300

    created Order model

---
+++ ticketing/payments/src/models/order.ts
@@ -0,0 +1,64 @@
+import { OrderStatus } from '@lpjtickets/common';
+import mongoose from 'mongoose';
+
+// An interface that describes the properties that are required to create a new order
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  status: OrderStatus;
+  price: number;
+}
+
+// An interface that describes the properties that the order model has
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+// An interface that describes the properties that a Order Document has
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  status: OrderStatus;
+  price: number;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
