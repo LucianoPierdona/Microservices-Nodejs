1f6e5e93b89eb71d7e0cbf9481c896b9a264c5ef
LucianoPierdona <lucianompj1@gmail.com>
2021-01-28 23:09:45 -0300

    stripe initial setup

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -8,6 +8,7 @@ import {
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -27,9 +28,16 @@ router.post(
     if (order.userId !== req.currentUser.id) {
       throw new NotAuthorizedError();
     }
+
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for an cancelled order');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
 
     return res.send({ success: true });
   }
