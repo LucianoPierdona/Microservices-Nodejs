1481e365ed57646ee1392ce02c84d363e40bef26
LucianoPierdona <lucianompj1@gmail.com>
2021-01-28 22:46:38 -0300

    finished new test

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -1,4 +1,10 @@
-import { requireAuth } from '@lpjtickets/common';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from '@lpjtickets/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
@@ -10,6 +16,21 @@ router.post(
   requireAuth,
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
     return res.send({ success: true });
   }
 );
